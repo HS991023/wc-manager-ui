e17c1498b478263e4ff107b561fe3cd46c410d0a
chenfangchao <socrates11@foxmail.com>
2022-06-09 22:58:17 +0800

    新增批量删除按钮

--- src/pages/user/UserInfo.vue
+++ src/pages/user/UserInfo.vue
@@ -31,6 +31,11 @@
     </el-input>
     <el-button class="serach-button" type="primary" icon="el-icon-search">搜索</el-button>
     </div>
+    <!-- 操作数据按钮区域 -->
+    <div class="operator-button-region">
+      <el-button type="primary" class="operator-button" icon="el-icon-circle-plus">新增</el-button>
+      <el-button type="danger" class="operator-button" icon="el-icon-error">批量删除</el-button>
+    </div>
     <div class="table-data">     
     <!-- 表单   -->
     <el-dialog
