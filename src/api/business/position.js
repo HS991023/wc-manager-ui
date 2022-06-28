dea10fe13655243ad19165d1efc032ad5b19946e
chenfangchao <socrates11@foxmail.com>
2022-06-28 21:48:50 +0800

    [add]#新增坑位管理模块

---
+++ src/api/business/position.js
@@ -0,0 +1,51 @@
+import request from '@/utils/request'
+
+
+//查询坑位列表
+export function listPosition(data) {
+    return request({
+        url: '/api/system/position/list',
+        method: 'post',
+        data: data
+    });
+}
+
+//查询坑位详情
+export function positionInfo(id) {
+    return request({
+        url: '/api/system/position/detail',
+        method: 'get',
+        params: {
+            id: id
+        }
+    });
+}
+
+//新增坑位信息
+export function addPosition(data) {
+    return request({
+        url: '/api/system/save/position',
+        method: 'post',
+        data: data
+    });
+}
+
+//更新坑位信息
+export function updatePosition(data) {
+    return request({
+        url: '/api/system/update/position',
+        method: 'put',
+        data: data
+    });
+}
+
+//删除坑位(逻辑删除)
+export function removePosition(ids) {
+    return request({
+        url: '/api/system/remove/positions',
+        method: 'delete',
+        params: {
+            ids: ids
+        }
+    });
+}
--- src/pages/position/PositionInfo.vue
+++ src/pages/position/PositionInfo.vue
@@ -6,128 +6,276 @@
     </div> 
     <div class="serach-input">
     <label class="serach-propties">隶属公厕:</label>    
-    <el-input
-        placeholder="请输入隶属公厕"
-        suffix-icon="el-icon-text"
-        >
-    </el-input>
+    <el-input placeholder="请输入隶属公厕" suffix-icon="el-icon-text"/>
     <label class="serach-propties">坑位序号:</label>    
-    <el-input
-        placeholder="请输入坑位序号"
-        suffix-icon="el-icon-text"
-        >
-    </el-input>
-    <el-button class="serach-button" type="primary" icon="el-icon-search">搜索</el-button>
+    <el-input placeholder="请输入坑位序号" suffix-icon="el-icon-text"/>
+    <!-- 搜索按钮区域 -->
+    <div class="serach-button-region"> 
+        <el-button class="serach-button" type="primary" icon="el-icon-search" @click="getPositionList()">搜索</el-button>
+        <el-button type="primary" class="serach-button" icon="el-icon-error" @click="getPositionListReset()">重置</el-button>
+    </div>
+    </div>
+   <!-- 操作数据按钮区域 -->
+    <div class="operator-button-region">
+      <el-button type="primary" class="operator-button" icon="el-icon-circle-plus" @click="handleAddPosition();dialogFormVisible=true">新增</el-button>
+      <el-button type="danger" class="operator-button" icon="el-icon-error" @click="handleDeletePosition()">批量删除</el-button>
+    </div>
+    <div class="form-data">
+    <!-- 表单新增或编辑对话框   -->
+    <el-dialog title="坑位信息" :visible.sync="dialogFormVisible">
+      <el-form :model="form" ref="form">
+        <el-form-item label="坑位名称" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="坑位编码" :label-width="formLabelWidth">
+          <el-input v-model="form.number" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="隶属公厕" :label-width="formLabelWidth">
+          <el-input v-model="form.type" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="坑位状态" :label-width="formLabelWidth">
+          <el-input v-model="form.status" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="绑定设备" :label-width="formLabelWidth">
+          <el-input v-model="form.location" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <div class="from-button-region" v-if="showFormButton">
+        <el-button class="button" @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" class="button" @click="submitForm()">确 定</el-button>
+      </div>
+      </div>
+    </el-dialog>
     </div>
     <!-- 表格组件 -->
     <div class="table-data"> 
-    <el-table :data="tableData" style="width: 100%" :row-class-name="tableRowClassName">
-    <!-- 多选框 -->
-    <el-table-column
-      type="selection"
-      width="55">
-    </el-table-column>  
-    <!-- 坑位编码    -->
-    <el-table-column
-      label="坑位编码"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <!-- 隶属公厕 -->
-    <el-table-column
-      label="隶属公厕"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <!-- 地区位置 -->
-    <el-table-column
-      label="地区位置"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-     <!-- 是否绑定设备 -->
-     <el-table-column
-      label="是否绑定设备"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
-    <!-- 状态 -->
-     <el-table-column
-      label="状态"
-      width="180">
-      <template slot-scope="scope">
-        <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
-          <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
-          </div>
-        </el-popover>
-      </template>
-    </el-table-column>
+    <el-table :data="positionList" style="width: 100%" ref="multipleTable"  v-loading="loading">
+    <el-table-column type="selection" width="55"/>
+    <el-table-column label="坑位名称" width="180" prop="name" key="name"/>
+    <el-table-column label="坑位编码" width="180" prop="number" key="number"/>
+    <el-table-column label="隶属公厕" width="180"/>
+    <el-table-column label="地区位置" width="180"/>
+    <el-table-column label="是否绑定设备" width="180"/>
+    <el-table-column label="状态" width="60" prop="status" key="status"/>
     <el-table-column label="操作">
       <template slot-scope="scope">
-        <el-button
-          size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-        <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">删除</el-button>
-          <el-button
-          size="mini"
-          type="danger"
-          @click="handleDelete(scope.$index, scope.row)">状态</el-button>  
+        <el-button size="mini" @click="handleEditPosition(scope.row);dialogFormVisible=true">编辑</el-button>
+        <el-button size="mini" type="danger" @click="handleDeletePosition(scope.row)">删除</el-button>
       </template>
     </el-table-column>
     </el-table>
-    <!-- 分页组件 -->
-    <div class="pageHelper">
-    <el-pagination
-      layout="prev, pager, next"
-      :total="1000">
-    </el-pagination>
     </div>
+    <!-- 分页组件 -->
+    <div class="pageHelper" v-if="total !=0 && total>0">
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="this.data.pageNum"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="this.data.pageSize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
     </div>
     </div>   
 </template>
-</template>
 
 <script>
+import {listPosition,positionInfo,addPosition,updatePosition,removePosition} from '@/api/business/position'
 export default {
-    name:'PositionInfo'
+    name:'PositionInfo',
+    data(){
+      return{
+         //表格数据
+        positionList: null,
+        //表单参数
+        form:{},
+        //总数
+        total:null,
+        //分页参数
+        data:{
+           pageNum: 1,
+           pageSize: 10,
+          
+        },
+        //坑位ID列表
+        ids:[],
+        formLabelWidth: '120px',
+        //是否加载中
+        loading: true,
+        dialogTableVisible: false,
+        //是否关闭表单对话框
+        dialogFormVisible: false,
+        //溢出时隐藏
+        showOverflowTooltip:true,
+        //是否表单展示取消确定按钮
+        showFormButton: true,
+      }
+    },
+    methods:{
+      //获取坑位列表
+      getPositionList(){
+        let data= this.data;
+        listPosition(data).then(response => {
+          if(response.count== 0){
+            this.total = response.count;
+            this.positionList = undefined;
+          }else{
+             this.positionList = response.data[0];
+             this.total = response.count;
+          }
+            this.loading = false;
+        }).catch(error=>{
+            console.log(error);
+        })
+      },
+      //坑位列表重置
+      getPositionListReset(){
+        let resetData= {
+           pageNum: 1,
+           pageSize: 10,
+        }
+        listPosition(resetData).then(response => {
+            this.positionList = response.data[0];
+            this.total = response.count;
+            this.loading = false;
+        });
+      },
+      //查询坑位详情
+      handlePositionInfo(id){
+        this.reset();
+        this.showFormButton = false
+        positionInfo(id).then(response=>{
+          this.form = response.data;
+        })
+      },
+      //新增坑位按钮
+      handleAddPosition(){
+        //重置表单
+        this.reset();
+        this.showFormButton = true;
+      },
+      //编辑坑位按钮
+      handleEditPosition(row) {
+        console.log(row);
+        //重置表单
+        this.reset();
+        this.form = this.handlePositionInfo(row.id);
+        this.showFormButton = true;
+      },
+      //提交表单
+      submitForm(){
+        this.$refs["form"].validate(valid => {
+        if (valid) {
+          //更新坑位
+          if (this.form.id != undefined) {
+            updatePosition(this.form).then(response =>{
+            if(response.code==200){
+             this.$msgbox('更新坑位信息成功', '系统提示', {
+                confirmButtonText: '确定',
+                type: 'warning'
+            });
+            this.dialogFormVisible = false;     
+            this.getToiletList();    
+            }
+            });
+          //新增坑位
+          }else{
+          addPosition(this.form).then(response =>{
+          if(response.code==200){
+             this.$msgbox('保存坑位信息成功', '系统提示', {
+                confirmButtonText: '确定',
+                type: 'warning'
+            });
+            this.dialogFormVisible = false;
+            this.getPositionList();     
+            }
+          });        
+          }
+        }});
+      },
+      //获取坑位ID 
+      handlePositionIds(val){
+         //批量ID
+         if(val instanceof Array){
+          this.ids = val.map(item=>{
+          return item.id;
+          });
+         }else{ 
+          //单个
+          if(val != undefined){
+            this.ids = val.id;
+          }
+          var rows = [];
+          rows.push(val);
+          this.toggleSelection(rows);
+         };
+      },
+      //删除坑位(逻辑删除)
+      handleDeletePosition() {
+        this.$confirm('此操作将删除坑位, 是否继续?', '系统提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          //发送删除请求
+          var positionIds = this.ids;
+          removePosition(positionIds.toString()).then(response=>{
+            if(response.code==200){
+               this.$message({
+                type: 'success',
+                message: '删除成功!'
+             });
+             this.getPositionList();
+            }
+          })
+        }).catch(() => {
+          //清除已选择的状态
+          this.toggleSelection();
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          }); 
+        });
+      },
+      //清除多选
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      //重置表单
+      reset() {
+        this.form={
+          id:undefined,
+          name:undefined,
+          number:undefined,
+          location:undefined,
+          type:undefined,
+          status:undefined
+        }
+      },
+      //更改每页大小
+      handleSizeChange(val) {
+        this.data.pageSize = val;
+        this.getPositionList();  
+      },
+      //更改当前页
+      handleCurrentChange(val) {
+        this.data.pageNum = val;
+        this.getPositionList(); 
+      }
+    },
+    created(){
+      this.getPositionList(); 
+    },
+    mounted(){
+    }
 }
 </script>
 
