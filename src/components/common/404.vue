cf129321a94417f54f4c20278327f8a29e8ee2f8
chenfangchao <socrates11@foxmail.com>
2022-07-20 15:03:42 +0800

    [update]#更新404&noauth页面

--- src/components/common/404.vue
+++ src/components/common/404.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        404
+    <div class="root">
+        <h1>404 网页找不到了 请联系管理员或检查输入的地址是否正确</h1>
     </div>
 </template>
 
@@ -24,6 +24,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+.root{
+    width: 100%;
+    height: 100%;
+}
+.root h1{
+    text-align: center;
+    margin-top: 10%;
+}
 </style>
--- src/components/common/NoAuth.vue
+++ src/components/common/NoAuth.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        暂无权限
+<div class="root">
+        <h1>当前登录用户无权限访问</h1>
     </div>
 </template>
 
@@ -24,6 +24,13 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+.root{
+    width: 100%;
+    height: 100%;
+}
+.root h1{
+    text-align: center;
+    margin-top: 10%;
+}
 </style>
